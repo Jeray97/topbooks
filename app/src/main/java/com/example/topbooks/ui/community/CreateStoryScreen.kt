7206aaeab76dcc21ad79e516e8480a58a1dbd6b3
Jeray <Jeray_bbt@hotmail.com>
2026-07-12 20:07:38 +0200

    chore: remove debug logs and design document

--- app/src/main/java/com/example/topbooks/ui/community/CreateStoryScreen.kt
+++ app/src/main/java/com/example/topbooks/ui/community/CreateStoryScreen.kt
@@ -231,19 +231,15 @@ fun CreateStoryScreen(
 
             Button(
                 onClick = {
-                    android.util.Log.d("STORY_DEBUG", "Botón clickeado - selectedBook: $selectedBook")
                     selectedBook?.let { book ->
-                        android.util.Log.d("STORY_DEBUG", "Creando historia para libro: ${book.id}")
                         viewModel.createStory(
                             bookId = book.id,
                             type = selectedType,
                             text = storyText,
                             backgroundColor = selectedColor,
-                            onSuccess = { 
-                                android.util.Log.d("STORY_DEBUG", "Historia creada exitosamente")
-                            }
+                            onSuccess = { }
                         )
-                    } ?: android.util.Log.e("STORY_DEBUG", "No hay libro seleccionado")
+                    }
                 },
                 modifier = Modifier
                     .fillMaxWidth()
